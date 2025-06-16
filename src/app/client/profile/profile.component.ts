625bce103d853ad867cda1ca4cf09c5027b7bd45
MaybeDH <hasselc081@gmail.com>
2025-06-16 10:44:55 -0400

    refactor: eliminar el servicio de perfil y simplificar la lógica de obtención de usuario en el componente de perfil

--- src/app/client/profile/profile.component.ts
+++ src/app/client/profile/profile.component.ts
@@ -1,15 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Profile} from '../../models/profile.model';
-import {ProfileService} from '../../core/services/profile/profile.service';
 import {AuthService} from '../../core/services/auth/auth.service';
-import {NgForOf, NgIf} from '@angular/common';
+import {NgForOf} from '@angular/common';
 import {User} from '../../models/user.model';
 import {UserService} from '../../core/services/user/user.service';
 
 @Component({
   selector: 'app-profile',
   imports: [
-    NgIf,
     NgForOf
   ],
   templateUrl: './profile.component.html',
@@ -23,14 +21,11 @@ import {UserService} from '../../core/services/user/user.service';
 
   constructor(
     private auth: AuthService,
-    private userService: UserService,
-    private profileService: ProfileService
+    private userService: UserService
   ) {}
 
   ngOnInit(): void {
     const currentUser = this.auth.getCurrentUser();
-    console.log('Current user', currentUser);
-
     const userId = currentUser?.id;
     console.log('Current user', userId);
     if (userId) {
@@ -39,50 +34,9 @@ import {UserService} from '../../core/services/user/user.service';
         console.log('User', data);
       });
     }
-    const profileId = currentUser?.profile?.profileId;
-    if(profileId) {
-      this.profileService.getProfileById(profileId).subscribe(data => {
-        this.profile = data;
-        console.log(this.profile);
-      })
-
-    }
-
-
-
   }
   get cuentasActivas() {
     return this.user?.accounts?.filter(cuenta => cuenta.status === 'Activa') || [];
   }
 }
 
-
-
-// constructor(private auth: AuthService,
-  //             private profileService: ProfileService,
-  //             private userService: UserService) {
-  // }
-  //
-  // ngOnInit() {
-  //   const  currentUser = this.auth.getCurrentUser();
-  //   console.log('Current user', currentUser);
-  //   const profileId = currentUser?.profile?.profileId;
-  //   if(profileId) {
-  //     this.profileService.getProfileById(profileId).subscribe(data => {
-  //       this.profile = data;
-  //       console.log(this.profile);
-  //     })
-  //
-  //   }
-  //   const userId = currentUser?.userId;
-  //   console.log(userId, "pro que no imprime");
-  //   if(userId) {
-  //     this.userService.getUserById(userId).subscribe(data => {
-  //       this.user = data;
-  //
-  //       console.log("user", data);
-  //     })
-  //   }
-  // }
-
-// }
