8ebf2be0f21c1887984f9f434d9e88d827e69f97
MaybeDH <hasselc081@gmail.com>
2025-06-16 07:40:06 -0400

    feat: agregar módulo de autenticación y componente de inicio de sesión con estilos y pruebas

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login',
+    loadComponent:()=>import('./login/login.component')
+    },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
