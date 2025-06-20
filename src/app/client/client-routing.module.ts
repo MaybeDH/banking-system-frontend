5e9492a2497ef35075fbcc1d4026dcda5e10f1bc
MaybeDH <hasselc081@gmail.com>
2025-06-20 19:47:16 -0400

    feat: eliminar la ruta del componente de detalle de cuenta

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -5,14 +5,12 @@ import {AccountsComponent} from './accounts/accounts.component';
 import {TransferComponent} from './transfer/transfer.component';
 import {BeneficiariesComponent} from './beneficiaries/beneficiaries.component';
 import {NewBeneficiaryComponent} from './beneficiaries/new-beneficiary/new-beneficiary.component';
-import {AccountDetailComponent} from './accounts/account-detail/account-detail.component';
 import {ProfileComponent} from './profile/profile.component';
 
 const routes: Routes = [
 
   { path: 'dashboard', component: DashboardComponent },
   { path: 'accounts', component: AccountsComponent },
-  { path: 'accounts/accountDetail/:id', component: AccountDetailComponent },
   { path: 'transfer', component: TransferComponent },
   { path: 'beneficiaries', component: BeneficiariesComponent },
   { path: 'beneficiaries/new', component: NewBeneficiaryComponent },
